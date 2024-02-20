f53c4fcf96897e830656135c7e3b6cc90d0aa85c
sangmin han <sangriel@naver.com>
2024-02-20 23:07:30 +0900

    welcom page

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
+	// 스프링 부트는 톰캣을 내장하고 있기 때문에, 자체적을 띄워주면서 스프링 부트가 같이 올라온다.
+	// 보통 프로젝트가 그래들을 사용해서 빌드를 하는 경우가 있는데, 이때 설정에서 ide를 쓰도록 하면 빌드속도가 빨라진다.
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
