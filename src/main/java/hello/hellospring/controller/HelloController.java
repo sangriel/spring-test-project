30f93fa6feb921087b6aff67a0a1af061981b227
sangmin han <sangriel@naver.com>
2024-02-20 23:45:22 +0900

    make RequestParam

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,13 @@ public class HelloController {
         //컨틀롤러에서 문자값 리턴시 viewresolver가 화면을 찾아서 처리한다.
         //hello.html
         return "hello";
+    }
+
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
