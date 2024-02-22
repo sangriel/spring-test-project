2123062d6e74f6e50e6f9dadb8b785fc9b292499
sangriel <39114237+sangriel@users.noreply.github.com>
2024-02-22 20:29:15 +0900

    스프링 강의  2차시  (#1)
    * 정적 컨텐츠 생성,
    
    * make RequestParam
    
    * @ResponseBody를 하고 return을 하면 json형식으로 변환된다.
    
    * @ResponseBody 어노테이션과  HttpMessageConverter

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,5 +17,44 @@ public class HelloController {
         //컨틀롤러에서 문자값 리턴시 viewresolver가 화면을 찾아서 처리한다.
         //hello.html
         return "hello";
+    }
+
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody //http body에 응답을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        //view 랑 템플릿 엔진을 거치지 않고 그냥 string으로 그대로 내려줌
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        //이렇게 하면 결과로 json format으로 떨어짐
+        //@ResponsBody가 붙어 있으면 view를 찾지 않고 HttpMessageConverter(Json Converter, String Converter)를 통해서 반환됨
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
