31b6ad8527e310ef5b28897adada98644a92999d
sangmin han <sangriel@naver.com>
2024-02-22 21:25:08 +0900

    command shift t 꿀기능, member service test case 작성

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    void join() {
+        //give
+        Member member = new Member();
+        member.setName("test");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member result = memberService.findOne(saveId).get();
+
+        assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    void joinDuplicateMemberException() {
+        //give
+        Member member1 = new Member();
+        member1.setName("test");
+
+        Member member2 = new Member();
+        member2.setName("test");
+
+        //when
+        Long mem1SaveId = memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+        // throw message까지 검증을 원하면 IllegalStateException e = 로 exception 받고 에러 메세지 비교하면 됨
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
