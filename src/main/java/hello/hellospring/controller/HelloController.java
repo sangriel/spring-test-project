e1c50f0890537252319b611921998b3c4ca5b41f
sangmin han <sangriel@naver.com>
2024-02-22 20:20:46 +0900

    @ResponseBody를 하고 return을 하면 json형식으로 변환된다.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,7 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +25,35 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody //http body에 응답을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        //view 랑 템플릿 엔진을 거치지 않고 그냥 string으로 그대로 내려줌
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        //이렇게 하면 결과로 json format으로 떨어짐
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
