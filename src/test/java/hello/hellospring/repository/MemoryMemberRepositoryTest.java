0ea8c16af57735746bbd96555bdf27a0af7913a9
sangmin han <sangriel@naver.com>
2024-02-22 20:50:16 +0900

    static import 는 남발해도 될까?

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -4,6 +4,8 @@ import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
@@ -16,6 +18,10 @@ class MemoryMemberRepositoryTest {
         repository.save(member);
 
         Member result = repository.findById(member.getId()).get();
-        Assertions.assertEquals(result, member);
+//        Assertions.assertEquals(result, member);
+        //요즘은 아래께 대세인가봄?
+        //option enter치면 static import되는 개쩌는 기능이?
+        //성능상의 하자나 그런건 아예 없는가?
+        assertThat(member).isEqualTo(result);
     }
 }
