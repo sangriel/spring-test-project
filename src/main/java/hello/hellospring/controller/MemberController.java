51a752032584dbdc988f0780d9303316a2b95d90
sangriel <39114237+sangriel@users.noreply.github.com>
2024-02-24 23:10:41 +0900

    4차시 (#3)
    * AutoWired를 이용한 spring framework DI
    
    * 컴포넌트 스캔 지원범위는 @SpringBootApplication 선언되어 있는 디렉토리 아래로만 가능
    
    * 자바코드로 직접 컨테이터 등록하는 방법
    
    * 4차시 끝

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired //스프링 컨테이너에서 자동적으로 memberservice를 가져오는 어노테이션 @Service라는 붙어 있는 클라스여야 가능함
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
     //service class의 경우 함수 이름을 개발적인 용어보다 진짜 비즈니스 적이고, 기획?스러운 네이밍을 하는 게 좋다고 한다.
 
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
